b25149f6b1b9f780b7f98872022c55f654b02c33
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2022-12-12 21:43:58 +0900

    프로젝트 생성 및 Room, acc-viewModel, dataBinding 추가
    1. 프로젝트 생성
    2. gitignore android 추가
    3. notes 리사이클러뷰 구현
       - notes adapter 추가
       - 전체 메모 리스트 뷰
    4. Room DB 사용
       - 메모 데이터 저장 기능
    5. repository, viewmodel 생성
       - 회전 시 데이터 값이 변경하지 않도록 UI컨트롤러에서 데이터 분리

---
+++ app/src/main/java/com/hy0417sage/inotes/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.hy0417sage.inotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hy0417sage.inotes.databinding.ActivityMainBinding
+import com.hy0417sage.inotes.room.NotesViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val notesAdapter: NotesAdapter = NotesAdapter()
+
+    private val notesViewModel by viewModels<NotesViewModel> {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return NotesViewModel(application) as T
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+            val goANoteActivity = Intent(this, ANoteActivity::class.java)
+            startActivity(goANoteActivity)
+        }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = notesAdapter
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+
+        notesViewModel.getNotesData().observe(this, Observer { loadNotes ->
+            notesAdapter.updateNotes(loadNotes)
+        })
+    }
+}
